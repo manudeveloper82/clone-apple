d8de3d789357fc15ae4575f301751e7ce9980122
Manuela Gomes <manudeveloper82@gmail.com>
2023-05-05 14:01:50 -0300

    correção

--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -1,20 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class HomepageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-    this.burgerMenuClick();
-  }
-
-  burgerMenuClick() {
-
-  }
+export class MenuComponent {
 
 }
+
+
+
+
