12148e20184619505ee6830b69874795d5a279ad
Manuela Gomes <manudeveloper82@gmail.com>
2023-06-05 15:37:54 -0300

    ajustes carrossel

--- src/app/component/carrossel/carrossel.component.ts
+++ src/app/component/carrossel/carrossel.component.ts
@@ -1,12 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+
 
 interface CarouselItem {
   link: string;
   image: string;
   description:string;
   title: string;
-  valor:string
+  value:string;
+  bgcolor: string;
 }
 
 @Component({
@@ -14,102 +16,111 @@ interface CarouselItem {
   templateUrl: './carrossel.component.html',
   styleUrls: ['./carrossel.component.css']
 })
-export class CarrosselComponent {
-  titulo: string = 'As novidades';
-  subtitulo: string = 'Veja o que acabou de chegar.';
+export class CarrosselComponent{
+  title: string = 'As novidades';
+  description: string = 'Veja o que acabou de chegar.';
 
-  carouselItems: CarouselItem[] = [
+   carouselItems: CarouselItem[] = [
     {
       link: "https://www.apple.com/br/shop/buy-iphone/iphone-14-pro",
       image: "../../../assets/img/carrossel/6phones.jpg",
       description:"IPHONE 14",
       title: "Maravilindo.",
-      valor:"A partir de R$ 7.599"
+      value:"A partir de R$ 7.599",
+      bgcolor: "white"
     },
     {
       link:"https://www.apple.com/br/shop/buy-ipad/ipad",
       image:"../../../assets/img/carrossel/iMac.jpg",
       description:"IPAD",
       title:"Inspira. Desenha. Diverte",
-      valor:"A partir de R$ 5.299"
+      value:"A partir de R$ 5.299",
+      bgcolor: "white"
     },
     {
       link:"https://www.apple.com/br/shop/buy-watch/apple-watch",
       image:"../../../assets/img/carrossel/8relogio.jpg",
       description:"APPLE WATCH SE",
       title:"Para amar cada segundo.",
-      valor:"A partir de R$ 3.399"
+      value:"A partir de R$ 3.399",
+      bgcolor: "white"
     },
     {
       link:"https://www.apple.com/br/shop/buy-iphone/iphone-14-pro",
-      image:"../../../assets/img/carrossel/4phone.jpg",
+      image:"../../../assets/img/carrossel/4phone.jpg" ,
       description:"IPHONE 14 PRO",
       title:"Pro. E além.",
-      valor:"A partir de R$ 9.599"
+      value:"A partir de R$ 9.599",
+      bgcolor: "black"
     },
     {
       link:"https://www.apple.com/br/shop/buy-ipad/ipad-pro",
       image:"../../../assets/img/carrossel/iPadPro.jpg",
       description:"IPAD PRO",
       title:"Com a potência do M2.",
-      valor:"A partir de R$ 9.799"
+      value:"A partir de R$ 9.799",
+      bgcolor: "black"
     },
     {
       link:"https://www.apple.com/br/shop/buy-watch/apple-watch-ultra",
       image:"../../../assets/img/carrossel/3relógio.jpg",
       description:"APPLE WATCH ULTRA",
       title:"A aventura te espera.",
-      valor:"A partir de R$ 10.299"
+      value:"A partir de R$ 10.299",
+      bgcolor: "white"
     },
     {
       link:"https://www.apple.com/br/shop/buy-ipad/ipad-pro",
       image:"../../../assets/img/carrossel/iPad.jpg",
       description:"MACBOOK PRO DE 14 E 16 POL.",
       title:"Desbrava fronteiras. Derruba barreiras.",
-      valor:"A partir de R$ 23.999"
+      value:"A partir de R$ 23.999",
+      bgcolor: "black"
     },
     {
       link:"https://www.apple.com/br/shop/buy-mac/mac-mini",
       image:"../../../assets/img/carrossel/MecMin.jpg",
       description:"MAC MINI",
       title:"Mais músculo. Mais cérebro.",
-      valor:"A partir de R$ 7.499"
+      value:"A partir de R$ 7.499",
+      bgcolor: "black"
     },
     {
       link:"https://www.apple.com/br/shop/watch/bands",
       image:"../../../assets/img/carrossel/pulseiras.jpg",
       description:"ACESSÓRIOS",
-      title:"Novas cores. <br> Todos os estilos.",
-      valor:"A partir de R$ 7.599"
+      title:"Novas cores. Todos os estilos.",
+      value:"A partir de R$ 7.599",
+      bgcolor: "white"
     },
-
 
   ];
 
   @ViewChild('carousel') carousel: any;
   customOptions: OwlOptions = {
     loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    nav: false,
+    dots: false,
     navSpeed: 700,
     navText: ['', ''],
+    margin: 10,
     responsive: {
       0: {
         items: 1
       },
       400: {
-        items: 2
+        items: 1
       },
       740: {
-        items: 3
+        items: 2
       },
       940: {
-        items: 4
+        items: 3
       }
     },
-    nav: true
   };
 
   prevSlide() {
@@ -119,4 +130,5 @@ export class CarrosselComponent {
   nextSlide() {
     this.carousel.next();
   }
+
 }
--- src/app/component/loja/loja.component.ts
+++ src/app/component/loja/loja.component.ts
@@ -1,19 +1,95 @@
 import { Component, ViewChild } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 
+interface CarouselItem {
+  link: string;
+  image: string;
+  description:string
+};
+
+interface ContatoItem {
+  link: string;
+  image: string;
+  description:string;
+  title: string;
+};
+
 @Component({
   selector: 'app-loja',
   templateUrl: './loja.component.html',
   styleUrls: ['./loja.component.css']
 })
 export class LojaComponent {
+  span:string='Loja'
+  title: string ='O melhor jeito de'
+  description: string ='comprar o que você ama.'
+
+  contatoItems: ContatoItem[] = [
+    {
+      link: "https://contactretail.apple.com/Help",
+      image: "../../../assets/img/lola e logo/perfil.jpg",
+      description:"Fale com um especialista.",
+      title: "Precisa de ajuda para comprar?",
+    },
+    {
+      link:"Fale com um especialista.",
+      image:"../../../assets/img/lola e logo/chatt.jpeg",
+      description:"Visite uma loja perto de você",
+      title:"Visite uma Apple Store",
+    }
+  ];
+
+
+  carouselItems: CarouselItem[] = [
+    {
+      link: "https://www.apple.com/br/shop/buy-mac",
+      image: "../../../assets/img/lola e logo/monitor.png",
+      description:"Mac"
+    },
+    {
+      link:"https://www.apple.com/br/shop/buy-iphone",
+      image:"../../../assets/img/lola e logo/quatrocell.png",
+      description:"iPhone"
+    },
+    {
+      link:"https://www.apple.com/br/shop/buy-ipad",
+      image:"../../../assets/img/lola e logo/iped.png",
+      description:"iPad"
+    },
+    {
+      link:"https://www.apple.com/br/shop/buy-watch",
+      image:"../../../assets/img/lola e logo/relógio.png",
+      description:"Apple Watch"
+    },
+    {
+      link:"https://www.apple.com/br/airpods/",
+      image:"../../../assets/img/lola e logo/airpods.png",
+      description:"AirPods"
+    },
+    {
+      link:"https://www.apple.com/br/shop/accessories/all/airtag",
+      image:"../../../assets/img/lola e logo/raster.png",
+      description:"AirTag"
+    },
+    {
+      link:"https://www.apple.com/br/apple-tv-4k",
+      image:"../../../assets/img/lola e logo/controle.png",
+      description:"Apple TV 4K"
+    },
+    {
+      link:"https://www.apple.com/br/shop/accessories/all",
+      image:"../../../assets/img/lola e logo/doisiphones.png",
+      description:"Acessórios"
+    },
+  ];
+
   @ViewChild('carousel') carousel: any;
   customOptions: OwlOptions = {
-    loop: true,
+    loop: false,
     mouseDrag: false,
     touchDrag: false,
     pullDrag: false,
-    dots: true,
+    dots: false,
     navSpeed: 700,
     navText: ['', ''],
     responsive: {
@@ -21,16 +97,16 @@ export class LojaComponent {
         items: 1
       },
       400: {
-        items: 2
-      },
-      740: {
         items: 3
       },
-      940: {
+      740: {
         items: 4
+      },
+      940: {
+        items: 6
       }
     },
-    nav: true
+    nav: false
   }
 
   prevSlide() {
