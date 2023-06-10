bbaac268f0ee3612f4db4250d54feae5f1dc2096
Manuela Gomes <manudeveloper82@gmail.com>
2023-06-10 02:10:27 -0300

    colocando mais itens no carrossel

--- src/app/component/carrossel-a/carrossel-a.component.ts
+++ src/app/component/carrossel-a/carrossel-a.component.ts
@@ -5,7 +5,7 @@ interface CarouselItem {
   link: string;
   image: string;
   title: string;
-  value:string;
+  value: string;
   bottom: string;
 }
 
@@ -15,111 +15,110 @@ interface CarouselItem {
   styleUrls: ['./carrossel-a.component.css']
 })
 export class CarrosselAComponent {
+  title: string = 'As novidades';
+  description: string = 'Veja o que acabou de chegar.';
 
-    title: string = 'As novidades';
-    description: string = 'Veja o que acabou de chegar.';
+  carouselItems: CarouselItem[] = [
+  {
+    link: "https://www.apple.com/br/shop/accessories/all/seasonal-colors",
+    image: "../../../assets/img/carrossel-a/store-card-40-accessories-202303_GEO_BR.jpeg",
+    title: "Novas cores",
+    value:"Para seus acessórios favoritos lançarem tendências.",
+    bottom: "top"
+  },
+  {
+    link:"https://www.apple.com/br/shop/buy-ipad/ipad",
+    image:"../../../assets/img/carrossel-a/MQU73.jpg",
+    title:"Site em andamento",
+    value:"",
+    bottom: "bottom"
+  },
+  {
+    link:"",
+    image:"",
+    title:"",
+    value:"",
+    bottom: ""
+  },
+  {
+    link:"",
+    image:"" ,
+    title:"",
+    value:"",
+    bottom: ""
+  },
+  {
+    link:"",
+    image:"",
+    title:"",
+    value:"",
+    bottom: ""
+  },
+  {
+    link:"",
+    image:"",
+    title:"",
+    value:"",
+    bottom: ""
+  },
+  {
+    link:"",
+    image:"",
+    title:"",
+    value:"",
+    bottom: ""
+  },
+  {
+    link:"",
+    image:"",
+    title:"",
+    value:"",
+    bottom:""
+  },
+  {
+    link:"",
+    image:"",
+    title:"",
+    value:"",
+    bottom: ""
+  },
 
-     carouselItems: CarouselItem[] = [
-      {
-        link: "https://www.apple.com/br/shop/accessories/all/seasonal-colors",
-        image: "../../../assets/img/carrossel-a/store-card-40-accessories-202303_GEO_BR.jpeg",
-        title: "Novas cores",
-        value:"Para seus acessórios favoritos lançarem tendências.",
-        bottom: "top"
-      },
-      {
-        link:"https://www.apple.com/br/shop/buy-ipad/ipad",
-        image:"../../../assets/img/carrossel-a/MQU73.jpg",
-        title:"Site em andamento",
-        value:"",
-        bottom: "bottom"
-      },
-      {
-        link:"",
-        image:"",
-        title:"",
-        value:"",
-        bottom: ""
-      },
-      {
-        link:"",
-        image:"" ,
-        title:"",
-        value:"",
-        bottom: ""
-      },
-      {
-        link:"",
-        image:"",
-        title:"",
-        value:"",
-        bottom: ""
-      },
-      {
-        link:"",
-        image:"",
-        title:"",
-        value:"",
-        bottom: ""
-      },
-      {
-        link:"",
-        image:"",
-        title:"",
-        value:"",
-        bottom: ""
-      },
-      {
-        link:"",
-        image:"",
-        title:"",
-        value:"",
-        bottom:""
-      },
-      {
-        link:"",
-        image:"",
-        title:"",
-        value:"",
-        bottom: ""
-      },
+];
 
-    ];
+  @ViewChild('carousel') carousel: any;
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    nav: false,
+    dots: false,
+    margin: -70,
+    navSpeed: 700,
+    navText: ['', ''],
 
-    @ViewChild('carousel') carousel: any;
-    customOptions: OwlOptions = {
-      loop: false,
-      mouseDrag: true,
-      touchDrag: true,
-      pullDrag: true,
-      nav: false,
-      dots: false,
-      margin: -70,
-      navSpeed: 700,
-      navText: ['', ''],
-
-      responsive: {
-        0: {
-          items: 1
-        },
-        400: {
-          items: 1
-        },
-        640: {
-          items: 3
-        },
-        940: {
-          items: 3
-        }
+    responsive: {
+      0: {
+        items: 1
       },
-    };
+      400: {
+        items: 1
+      },
+      640: {
+        items: 3
+      },
+      940: {
+        items: 3
+      }
+    },
+  };
 
-    prevSlide() {
-      this.carousel.prev();
-    }
-
-    nextSlide() {
-      this.carousel.next();
-    }
-
+  prevSlide() {
+    this.carousel.prev();
   }
+
+  nextSlide() {
+    this.carousel.next();
+  }
+
+}
--- src/app/component/carrossel/carrossel.component.ts
+++ src/app/component/carrossel/carrossel.component.ts
@@ -22,6 +22,54 @@ export class CarrosselComponent{
 
    carouselItems: CarouselItem[] = [
     {
+      link:"https://www.apple.com/br/shop/buy-mac/macbook-air",
+      image:"../../../assets/img/carrossel/nottop.jpeg",
+      description:"MACBOOK AIR DE 15 POLEGADAS",
+      title:"Espaçoso sem perder a fineza.",
+      value:"A partir de R$ 14.999",
+      bgcolor: "white"
+    },
+    {
+      link:"https://www.apple.com/br/shop/buy-ipad/ipad-air",
+      image:"../../../assets/img/carrossel/tab vent.jpeg",
+      description:"IPAD AIR",
+      title:"Cor e potência no ar.",
+      value:"A partir de R$ 6.999",
+      bgcolor: "black"
+    },
+    {
+      link:"https://www.apple.com/br/shop/product/MRTL3AM/A/pulseira-esportiva-edi%C3%A7%C3%A3o-orgulho-para-caixa-de-41-mm-p-m",
+      image:"../../../assets/img/carrossel/relogio mix.jpeg",
+      description:"PULSEIRA ESPORTIVA EDIÇÃO ORGULHO",
+      title:"O orgulho vai se espalhar.",
+      value:"A partir de R$ 499",
+      bgcolor: "white"
+    },
+    {
+      link:"https://www.apple.com/br/shop/buy-iphone/iphone-14-pro",
+      image:"../../../assets/img/carrossel/4phone.jpg" ,
+      description:"IPHONE 14 PRO",
+      title:"Pro. E além.",
+      value:"A partir de R$ 9.599",
+      bgcolor: "black"
+    },
+    {
+      link:"https://www.apple.com/br/shop/buy-mac/mac-studio",
+      image:"../../../assets/img/carrossel/m2max.jpeg",
+      description:"NAC STUDIO",
+      title:"Com a potência do M2 Max e M2 Ultra.",
+      value:"A partir de R$ 22.999",
+      bgcolor: "white"
+    },
+    {
+      link:"https://www.apple.com/br/shop/buy-ipad/ipad-pro",
+      image:"../../../assets/img/carrossel/iPad.jpg",
+      description:"MACBOOK PRO DE 14 E 16 POL.",
+      title:"Desbrava fronteiras. Derruba barreiras.",
+      value:"A partir de R$ 23.999",
+      bgcolor: "black"
+    },
+    {
       link: "https://www.apple.com/br/shop/buy-iphone/iphone-14-pro",
       image: "../../../assets/img/carrossel/6phones.jpg",
       description:"IPHONE 14",
@@ -46,14 +94,6 @@ export class CarrosselComponent{
       bgcolor: "white"
     },
     {
-      link:"https://www.apple.com/br/shop/buy-iphone/iphone-14-pro",
-      image:"../../../assets/img/carrossel/4phone.jpg" ,
-      description:"IPHONE 14 PRO",
-      title:"Pro. E além.",
-      value:"A partir de R$ 9.599",
-      bgcolor: "black"
-    },
-    {
       link:"https://www.apple.com/br/shop/buy-ipad/ipad-pro",
       image:"../../../assets/img/carrossel/iPadPro.jpg",
       description:"IPAD PRO",
@@ -68,14 +108,6 @@ export class CarrosselComponent{
       title:"A aventura te espera.",
       value:"A partir de R$ 10.299",
       bgcolor: "white"
-    },
-    {
-      link:"https://www.apple.com/br/shop/buy-ipad/ipad-pro",
-      image:"../../../assets/img/carrossel/iPad.jpg",
-      description:"MACBOOK PRO DE 14 E 16 POL.",
-      title:"Desbrava fronteiras. Derruba barreiras.",
-      value:"A partir de R$ 23.999",
-      bgcolor: "black"
     },
     {
       link:"https://www.apple.com/br/shop/buy-mac/mac-mini",
@@ -104,7 +136,7 @@ export class CarrosselComponent{
     pullDrag: true,
     nav: false,
     dots: false,
-    margin: -70,
+    margin: -96,
     navSpeed: 700,
     navText: ['', ''],
 
@@ -116,7 +148,7 @@ export class CarrosselComponent{
         items: 1
       },
       640: {
-        items: 3
+        items: 2
       },
       940: {
         items: 3
