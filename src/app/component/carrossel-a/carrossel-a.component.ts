58c50eaf39a5d109692d04e31d7b8d6a95e32b56
Manuela Gomes <manudeveloper82@gmail.com>
2023-06-13 20:47:31 -0300

    finalizado (componente educação)

--- src/app/component/carrossel-a/carrossel-a.component.ts
+++ src/app/component/carrossel-a/carrossel-a.component.ts
@@ -5,8 +5,8 @@ interface CarouselItem {
   link: string;
   image: string;
   title: string;
-  value: string;
-  bottom: string;
+  description:string;
+  bgcolor: string;
 }
 
 @Component({
@@ -15,75 +15,40 @@ interface CarouselItem {
   styleUrls: ['./carrossel-a.component.css']
 })
 export class CarrosselAComponent {
-  title: string = 'As novidades';
-  description: string = 'Veja o que acabou de chegar.';
+  title: string = 'É melhor com a Apple.';
+  description: string = 'Faça ainda mais com os produtos e serviços Apple.';
 
-  carouselItems: CarouselItem[] = [
-  {
-    link: "https://www.apple.com/br/shop/accessories/all/seasonal-colors",
-    image: "../../../assets/img/carrossel-a/store-card-40-accessories-202303_GEO_BR.jpeg",
-    title: "Novas cores",
-    value:"Para seus acessórios favoritos lançarem tendências.",
-    bottom: "top"
-  },
-  {
-    link:"https://www.apple.com/br/shop/buy-ipad/ipad",
-    image:"../../../assets/img/carrossel-a/MQU73.jpg",
-    title:"Site em andamento",
-    value:"",
-    bottom: "bottom"
-  },
-  {
-    link:"",
-    image:"",
-    title:"",
-    value:"",
-    bottom: ""
-  },
-  {
-    link:"",
-    image:"" ,
-    title:"",
-    value:"",
-    bottom: ""
-  },
-  {
-    link:"",
-    image:"",
-    title:"",
-    value:"",
-    bottom: ""
-  },
-  {
-    link:"",
-    image:"",
-    title:"",
-    value:"",
-    bottom: ""
-  },
-  {
-    link:"",
-    image:"",
-    title:"",
-    value:"",
-    bottom: ""
-  },
-  {
-    link:"",
-    image:"",
-    title:"",
-    value:"",
-    bottom:""
-  },
-  {
-    link:"",
-    image:"",
-    title:"",
-    value:"",
-    bottom: ""
-  },
+   carouselItems: CarouselItem[] = [
+    {
+      link:"https://tv.apple.com/?WOIsmapCoords=br/show/ted-lasso/umc.cmc.vtoh0mn0xn7t3c643xqonfzy",
+      image:"../../../assets/img/carrossel-a/store-card-50-tv-services-202303.jpeg",
+      description:"APPLE TV+",
+      title:"Ganhe três meses de Apple TV+ na compra de um aparelho Apple°.",
+      bgcolor: "blue"
+    },
+    {
+      link:"https://www.apple.com/br/apple-one/",
+      image:"../../../assets/img/carrossel-a/5-serviços-apple.jpeg",
+      description:"",
+      title:"Cinco serviços Apple. Uma só assinatura.",
+      bgcolor: "white"
+    },
+    {
+      link:"https://www.apple.com/br/apple-pay/",
+      image:"../../../assets/img/carrossel-a/apple-pay.jpeg",
+      description:"",
+      title:"Descubra todas as maneiras de usar o Apple Pay.",
+      bgcolor: "white"
+    },
+    {
+      link:"https://www.apple.com/br/home-app/",
+      image:"../../../assets/img/carrossel-a/casa.jpeg" ,
+      description:"CASA",
+      title:"Veja como um app pode controlar toda a sua casa.",
+      bgcolor: "white"
+    },
 
-];
+  ];
 
   @ViewChild('carousel') carousel: any;
   customOptions: OwlOptions = {
@@ -93,7 +58,7 @@ export class CarrosselAComponent {
     pullDrag: true,
     nav: false,
     dots: false,
-    margin: -70,
+    margin: 150,
     navSpeed: 700,
     navText: ['', ''],
 
@@ -105,7 +70,7 @@ export class CarrosselAComponent {
         items: 1
       },
       640: {
-        items: 3
+        items: 2
       },
       940: {
         items: 3
--- src/app/component/educacao/educacao.component.ts
+++ src/app/component/educacao/educacao.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+interface EspecialItem {
+  link: string;
+  image: string;
+  title: string;
+  description:string;
+  bgcolor: string;
+}
 @Component({
   selector: 'app-educacao',
   templateUrl: './educacao.component.html',
   styleUrls: ['./educacao.component.css']
 })
 export class EducacaoComponent {
+  title: string = 'É melhor com a Apple.';
+  description: string = 'Faça ainda mais com os produtos e serviços Apple.';
 
+  especialItems: EspecialItem[] = [
+    {
+      link:"https://tv.apple.com/?WOIsmapCoords=br/show/ted-lasso/umc.cmc.vtoh0mn0xn7t3c643xqonfzy",
+      image:"../../../assets/img/educacao/educação.jpeg",
+      description:"EDUCAÇÂO",
+      title:"Economize no Mac ou iPad com os preços especiais para a educação.",
+      bgcolor: "white"
+    },
+    {
+      link:"https://www.apple.com/br/apple-one/",
+      image:"../../../assets/img/educacao/negocios.jpeg",
+      description:"NEGÓCIOS",
+      title:"Seja qual for o tamanho da sua empresa, queremos trabalhar com você.",
+      bgcolor: "gray"
+    },
+
+  ];
 }
+
+
